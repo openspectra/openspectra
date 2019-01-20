1b592d1ada2663979770ffbc91a6bd13ed1795b6
Joseph M Conti <jconti13@gmail.com>
2019-01-19 17:33:15 -0700

    update requirements.txt and run.sh after Python and dependencies version upgrades

--- run.sh
+++ run.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
 #Run this script after activating (workon) the virtual env.
-#Install wxPython in the virtual env using pip install wxPython.
-
-#WXPYTHON_APP="helloWorld.py"
-PYVER=3.6
+PYVER=3.7
+PYSUBVER=3.7.2
 
 if [ -z "$VIRTUAL_ENV" ] ; then
     echo "You must activate your virtualenv: set '$VIRTUAL_ENV'"
     exit 1
 fi
 
-PYSUBVER=3.6.5
 
-BREW_PYTHON_ROOT="/usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/$PYVER"
-
+BREW_PYTHON_ROOT="/usr/local/Cellar/python/3.7.2_1/Frameworks/Python.framework/Versions/$PYVER"
 PYTHON_BINARY="bin/python$PYVER"
-
 FRAMEWORK_PYTHON="$BREW_PYTHON_ROOT/$PYTHON_BINARY"
 
 # Use the Framework Python to run the app
 export PYTHONHOME=$VIRTUAL_ENV
 
-echo $FRAMEWORK_PYTHON
+echo "Using Python framework:" $FRAMEWORK_PYTHON
+#echo "Using virtual environment:" $$VIRTUAL_ENV
 
-#exec "$FRAMEWORK_PYTHON" "./$WXPYTHON_APP" $*
 exec "$FRAMEWORK_PYTHON" "./$1" $*
