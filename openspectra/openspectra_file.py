bb6ecd278f43922b7746a48d2368f2650f6eac02
Joseph M Conti <jconti13@gmail.com>
2019-01-21 18:31:28 -0700

    add basic copyright notice to all source files

--- openspectra/openspectra_file.py
+++ openspectra/openspectra_file.py
@@ -1,3 +1,7 @@
+#  Developed by Joseph M. Conti and Joseph W. Boardman on 1/21/19 6:29 PM.
+#  Last modified 1/21/19 6:29 PM
+#  Copyright (c) 2019. All rights reserved.
+
 import logging
 from pathlib import Path
 import re
--- openspectra/ui/bandlist.py
+++ openspectra/ui/bandlist.py
@@ -1,3 +1,7 @@
+#  Developed by Joseph M. Conti and Joseph W. Boardman on 1/21/19 6:29 PM.
+#  Last modified 1/21/19 6:29 PM
+#  Copyright (c) 2019. All rights reserved.
+
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, QItemSelectionModel, QObject, QModelIndex
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QAbstractItemView, QTreeWidget, QTreeWidgetItem, QRadioButton, \
     QHBoxLayout, QPushButton
